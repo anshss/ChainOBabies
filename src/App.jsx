5ee70857a14f2528a7e074dda6ce15edf1f48b03
Sarthak Vaish <sarthakvaish184@gmail.com>
2022-06-26 14:48:04 +0530

    resolved alanAi bug

--- src/App.jsx
+++ src/App.jsx
@@ -10,31 +10,32 @@ const ALAN_STUDIO_API_KEY =
 function App() {
   const navigate = useNavigate();
   var greetingWasSaid = false;
-  const alanBtnInstance = alanBtn({
-    key: ALAN_STUDIO_API_KEY,
-    onButtonState: async function (status) {
-      if (status === "ONLINE") {
-        if (!this.greetingWasSaid) {
-          await alanBtnInstance.activate();
-          alanBtnInstance.playText("Hello! I'm Alan. How can I help you?");
-          this.greetingWasSaid = true;
-        }
-      }
-    },
-    onCommand: ({ command }) => {
-      if (command === "testCommand") {
-        alert("This commnad was executed sucessfully");
-      }
-      if (command === "go:back") {
-        navigate(-1);
-      }
-      if (command === "get:started") {
-        navigate("/getstarted");
-      }
-    },
-  });
   useEffect(() => {
-    alanBtnInstance;
+    var alanBtnInstance = alanBtn({
+      key: ALAN_STUDIO_API_KEY,
+      onButtonState: async function (status) {
+        if (status === "ONLINE") {
+          if (!this.greetingWasSaid) {
+            await alanBtnInstance.activate();
+            const isdone = alanBtnInstance.playText(
+              "Welcome to CHAIN'O BABIES, Click on the mic button below if you need my help."
+            );
+            this.greetingWasSaid = true;
+          }
+        }
+      },
+      onCommand: ({ command }) => {
+        if (command === "testCommand") {
+          alert("This commnad was executed sucessfully");
+        }
+        if (command === "go:back") {
+          navigate(-1);
+        }
+        if (command === "get:started") {
+          navigate("/getstarted");
+        }
+      },
+    });
   }, []);
 
   return (
