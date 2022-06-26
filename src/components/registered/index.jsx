f059dfddf4e81b5d07ae564427fcb3879075ff01
Ansh <anshsaxena4190@gmail.com>
2022-06-26 16:43:36 +0530

    ui update

---
+++ src/components/registered/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./index.scss";
+
+const Registered = () => {
+    return(
+        <div className="registered">
+            <h1> YOU ARE NOW REGISTERED ✅ </h1>
+        </div>
+    )
+}
+
+export default Registered;
