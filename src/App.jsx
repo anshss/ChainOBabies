6d80581afecead526df34371482b92ae88ac06e9
Sarthak Vaish <sarthakvaish184@gmail.com>
2022-06-26 10:28:27 +0530

    fixed some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import GetStarted from "./pages/GetStarted";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import { useEffect } from "react";
 import LandingPage from "./pages/landingPage";
-
-// alanBtn({
-//   key: "ALAN_STUDIO_API_KEY",
-//   rootEl: document.getElementById("alan-btn"),
-// });
+import { Route, Routes } from "react-router-dom";
 
 const ALAN_STUDIO_API_KEY =
   "f039e00acc09bb6be6f0bf4fca3358f52e956eca572e1d8b807a3e2338fdd0dc/stage";
@@ -23,14 +19,13 @@ function App() {
     });
   }, []);
   return (
-    <div class="">
-      <GetStarted />
+    <div>
+      <Routes>
+        <Route index element={<LandingPage />} />
+        <Route path="/getstarted" element={<GetStarted />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
-{
-  /* <GetStarted />; */
-}
-// w-full h-screen bg-gradient-to-tr from-[#f0d7eb] to-[#ddafd3] via-[#ebcbe3]
